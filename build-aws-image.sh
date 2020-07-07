faf4c619dc1105840bb17e5a96afce0cbcf4935a
Major Hayden <major@mhtx.net>
2020-07-07 07:50:54 -0500

    Do RHN registration in AWS

--- build-aws-image.sh
+++ build-aws-image.sh
@@ -30,6 +30,16 @@ gpgcheck=0
 priority=5
 EOF
 
+# Ensure RHEL 8.2 is registered.
+if [[ $OS_STRING == rhel82 ]]; then
+    greenprint "🪙 Registering RHEL 8"
+    subscription-manager register \
+        --username=$RHN_CREDS_USR --password=$RHN_CREDS_PSW \
+        --auto-attach
+    subscription-manager repos \
+        --enable=codeready-builder-for-rhel-8-x86_64-rpms
+fi
+
 # RHEL 8.3 content is not on the CDN yet, so use internal repositories.
 if [[ $OS_STRING == rhel83 ]]; then
     greenprint "🌙 Setting up RHEL 8.3 nightly repositories"
